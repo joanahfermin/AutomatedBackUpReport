856822d7e879231aed0ca20d18cfcbb6115d431a
Joanah Fermin <ferminjoanah@gmail.com>
2025-07-07 22:40:09 +0800

    Finished setup connection to the db, add migration for the HCTest table

---
+++ AutomatedBackUpReport/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AutomatedBackUpReport.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomatedBackUpReport.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<HCTest> hCTests { get; set; }
+    }
+}
