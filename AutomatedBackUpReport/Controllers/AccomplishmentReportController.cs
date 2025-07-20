c2e834868bec233d1a6fcacbf5c1480de9ff89d3
Joanah Fermin <ferminjoanah@gmail.com>
2025-07-20 22:51:17 +0800

    update full calendar with added hover effect on each day

---
+++ AutomatedBackUpReport/Controllers/AccomplishmentReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutomatedBackUpReport.Controllers
+{
+    public class AccomplishmentReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.month = DateTime.Now.Month;
+            ViewBag.year = DateTime.Now.Year;
+            ViewBag.Kamote = "abc";
+
+            return View();
+        }
+    }
+}
